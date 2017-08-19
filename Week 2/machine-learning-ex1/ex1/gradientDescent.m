6e9bef696ce4eea143b334076333a7d03a47aa21
Durgesh Samariya <samariya.durgesh@gmail.com>
2017-08-20 00:23:04 +1000

    Initial file implemented

--- Week 2/machine-learning-ex1/ex1/gradientDescent.m
+++ Week 2/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,12 +16,15 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    colums = size(X,2);
+    predictions = X * theta;
+    for j = 1:colums
+        
+        error = (predictions - y)' * X(:,j);
+        converge = (1/m) * sum(error);
+    
+        theta(j) = theta(j) - (alpha * converge);
+    end
 
     % ============================================================
 
